bf417963ae3de13f097f5ccf244cea24f191bd76
August Johnson <gusjohnson98@gmail.com>
2019-04-14 17:34:14 -0500

    css and js file path linked

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
+var express = require("express");
+var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 
 var app = express();
 
-require("./routes/apiRoutes.js")(app);
-
 var PORT = 3000;
+
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+require("./routes/apiRoutes.js")(app);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
